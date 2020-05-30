1193c637f723e47379b7d2e1eb1ed29bb6a87a81
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-05-30 13:48:09 +0000

    updated files

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var day = document.getElementById("day");
 var agevalue = age.value;
 var dayvalue = day.value;
 
- if ((age.value <= 11 && age.value >= 65) && (day.value == "Tuesday" || day.value == "Thursday" || day.value == "Saturday")) {
-   alert("You can get into the museum!");
+ if ((age.value < 18) && (day.value == "Monday" || day.value == "Tuesday" || day.value == "Wednesday" || day.value == "Thursday" || day.value == "Friday")) {
+   alert("Time for School!");
 }
- else if ((age.value >= 12 && age.value <= 64) && (day.value == "Monday" || day.value == "Wednesday" || day.value == "Friday")) {
-   alert("You can get into the museum!");
+else if ((age.value >= 18) && (day.value == "Monday" || day.value == "Tuesday" || day.value == "Wednesday" || day.value == "Thursday" || day.value == "Friday")) {
+   alert("Time to go to work!");
 }
  else {
-  alert("You cannot get into the museum!");
+  alert("Time to relax for the weekend!");
 }
 };
